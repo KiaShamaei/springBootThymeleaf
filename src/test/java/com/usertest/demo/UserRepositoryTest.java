d757ace21c4e1c99cee8ca9362ce9aae309bba39
kiarash.shamaii <kiarash.shamaii@gmail.com>
2021-10-31 08:54:22 +0330

    add test a

---
+++ src/test/java/com/usertest/demo/UserRepositoryTest.java
@@ -0,0 +1,37 @@
+package com.usertest.demo;
+
+
+import com.usertest.demo.users.User;
+import com.usertest.demo.users.UserRepository;
+
+import org.assertj.core.api.Assertions;
+import org.assertj.core.api.Condition;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+
+
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Rollback(false)
+public class UserRepositoryTest {
+    @Autowired private UserRepository repo ;
+    @Test
+    public  void TestAddNew(){
+        User user = new User();
+        user.setEmail("nima@gmail.com");
+        user.setPassword("1234");
+        user.setFirstName("nima");
+        user.setLastName("nimaee");
+        User savedUser = repo.save(user);
+        Assertions.assertThat(savedUser).isNotNull();
+        Assertions.assertThat(savedUser.getId()).isGreaterThan(0);
+    }
+//    public void testListAll() {
+//        Iterable<Users> users = repo.findAll();
+//        Assertions.assertThat(users).has(Condition<users> users.)
+//    }
+}
