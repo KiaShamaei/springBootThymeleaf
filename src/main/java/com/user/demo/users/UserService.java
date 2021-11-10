c3439ed28da1a99c7880c2fcb33d23e7f9a512bf
kiarash.shamaii <kiarash.shamaii@gmail.com>
2021-11-10 08:51:50 +0330

    get user method add

--- src/main/java/com/user/demo/users/UserService.java
+++ src/main/java/com/user/demo/users/UserService.java
@@ -2,9 +2,11 @@ package com.user.demo.users;
 
 
 
+import javassist.NotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -18,6 +20,14 @@ public class UserService {
     }
     public void saveUser(User user){
         repo.save(user);
+    }
+    public User get(int id) throws UserNotFoundException{
+        Optional <User> result = repo.findById(id);
+        if(result.isPresent()){
+            return result.get();
+        }else {
+            throw new UserNotFoundException("User not found base on this Id !");
+        }
     }
     public void deleteById(int id){  repo.deleteById(id);}
 }
