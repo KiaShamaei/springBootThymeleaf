5c62283d8902449671fe92e2c6c4ad4f86ebdf11
kiarash.shamaii@gmail.com <kiarash.shamaii@gmail.com>
2021-11-20 08:48:18 +0330

    edit user add

--- src/main/java/com/user/demo/UserController.java
+++ src/main/java/com/user/demo/UserController.java
@@ -2,6 +2,7 @@ package com.user.demo;
 
 
 import com.user.demo.users.User;
+import com.user.demo.users.UserNotFoundException;
 import com.user.demo.users.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -28,6 +29,7 @@ public class UserController {
     @GetMapping(value = "/user/new")
     public String showForm (Model model){
         model.addAttribute("user" , new User());
+        model.addAttribute("pageTitle" , "New User");
         return "newUserForm";
     }
     @PostMapping(value = "/user/save")
@@ -40,6 +42,20 @@ public class UserController {
     @GetMapping(value = "/user/delete/{id}")
     public void deleteUser(@PathVariable("id")  int id) {
         service.deleteById(id);
+    }
+    @GetMapping(value = "/user/edit/{id}")
+    public String  showEditForm(@PathVariable("id")  int  id , Model model ,RedirectAttributes ra){
+        try{
+            User user = service.get(id);
+            model.addAttribute("user" , user);
+            model.addAttribute("pageTitle" , "Edit user");
+            return "newUserForm";
+
+        } catch (UserNotFoundException e) {
+            ra.addFlashAttribute("message" , "addnew is successfully");
+            return "redirect:/users";
+        }
+
     }
 
 
